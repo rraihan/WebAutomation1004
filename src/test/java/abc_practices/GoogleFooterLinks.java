1a21a0a6d39b54abfc31e6086f1d3a76dc164b29
rraihan <39595199+rraihan@users.noreply.github.com>
2018-07-28 17:35:34 -0400

    change has been made

--- src/test/java/abc_practices/GoogleFooterLinks.java
+++ src/test/java/abc_practices/GoogleFooterLinks.java
@@ -12,7 +12,7 @@ public class GoogleFooterLinks {
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\rakib\\workspace\\WebAutomation1004\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://Google.com");
-		driver.manage().window().maximize();
+		
 		driver.findElement(By.xpath("//*[@id=\"fsr\"]/a[1]")).click();
 		String url = "https://youtu.be/ggoJFaE71W8";
 		driver.get(url);
