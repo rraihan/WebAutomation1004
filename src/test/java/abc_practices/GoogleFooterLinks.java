d1cbc92e272eb02edc13850cf58a4b07decd427a
rraihan <dialraihan@gmail.com>
2018-07-27 21:36:31 -0400

    video

---
+++ src/test/java/abc_practices/GoogleFooterLinks.java
@@ -0,0 +1,25 @@
+package abc_practices;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class GoogleFooterLinks {
+
+	public static void main(String[] args) throws Throwable {
+		 System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\rakib\\workspace\\WebAutomation1004\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://Google.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"fsr\"]/a[1]")).click();
+		String url = "https://youtu.be/ggoJFaE71W8";
+		driver.get(url);
+		driver.get("http://Google.com");
+		
+		
+		
+	}
+
+}
